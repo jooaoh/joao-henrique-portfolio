826761d6257c80cd3e1b5048be5714e30f6dce3b
João Henrique <joaohenrique.thole@gmail.com>
2025-07-23 21:59:20 -0300

    Update Streamlit app

--- site.py
+++ site.py
@@ -34,7 +34,7 @@ if st.session_state.page == "curriculo":
         st.write("Excel, Power BI, Python, R Studio")
         st.write("https://www.linkedin.com/in/jo%C3%A3o-henrique-thole-ribeiro-4355ba1ba/")
     with col2:
-        st.image("https://drive.google.com/file/d/1UNToL5q6IJKA6cZwzOENttlsmWmcLYYX/view?usp=drive_link", width=100)  # Ajustar para URL direta ou mover para assets
+        st.image("/Users/jooaoh/Desktop/Python Streamlit/assets/CorpJoaoH.jpeg", width=100)  # Ajustar para URL direta ou mover para assets
 
     # URL de visualização direta do PDF no Google Drive
     pdf_id = "168wtaEWKim5yEfDvIbAU1Rl4Mfke8wL7"
